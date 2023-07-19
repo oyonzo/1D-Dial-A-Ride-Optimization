773fda52b66e7f9ffc1d25ac14ce197e65622648
yb <95590361+oyonzo@users.noreply.github.com>
2023-07-19 11:33:05 -0400

    Moved the request generation methods into request.java

--- Project/src/Project/Request.java
+++ Project/src/Project/Request.java
@@ -1,6 +1,6 @@
 package Project;
 
-import java.util.List;
+import java.util.*;
 
 /*
  * Request class, for creating a request from data
@@ -16,6 +16,12 @@ public class Request implements Comparable<Request> {
 	double finishTime;
 	double speed = 1;
 	double f_val;
+	public static int numRequest;
+	public static int iterations;
+	// declare the Height and Width of our metric space
+	private static int WIDTH = 100;
+	private static int HEIGHT = 100;
+	private static int TLIMIT = 24;
 
 	// In reality, each request can be viewed as a request of starting position,
 	// finish position, and when to pick up
@@ -76,4 +82,64 @@ public class Request implements Comparable<Request> {
 				+ "f-value: " + f_val;
 	}
 
+	/*
+	 * Generate a random double within a range
+	 * 
+	 * Args:
+	 * rangeMax: Exclusive upper bound
+	 * rangeMin: Inclusive lower bound
+	 * 
+	 * Returns:
+	 * A random double
+	 */
+	private static double randDouble(double rangeMax, double rangeMin) {
+		Random r = new Random();
+		double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+		return randomValue;
+	}
+
+	/*
+	 * Create a random point with an x and y value within the constraints of the
+	 * metric space
+	 * 
+	 * Returns:
+	 * An Array containing the coordinates of the point [x,y]
+	 */
+	private static double[] randPoint() {
+		double x = randDouble(WIDTH, 0);
+		double y = randDouble(HEIGHT, 0);
+
+		double[] point = new double[] { x, y };
+		return point;
+	}
+
+	/*
+	 * Generates a random request in the metric space
+	 * 
+	 * Returns:
+	 * An array representing a request [startpos, finishpos, picktime]
+	 */
+	public static Request createRandomRequest() {
+		double[] s = randPoint();
+		double[] f = randPoint();
+		double pt = randDouble(TLIMIT, 0);
+
+		Request request = new Request(s, f, pt);
+
+		return request;
+	}
+
+	/*
+	 * Generates a list of random requests
+	 * 
+	 * Returns:
+	 * A list representing a request [startpos, finishpos, picktime]
+	 */
+	public static List<Request> createRequests() {
+		List<Request> requests = new ArrayList<Request>();
+		for (int i = 0; i < numRequest; i++) {
+			requests.add(createRandomRequest());
+		}
+		return requests;
+	}
 }
