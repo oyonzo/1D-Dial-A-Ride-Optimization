2ec5a1337fc406765ef8b42028c3efc2beff0c4f
Zeyi Tong <ztong@middlebury.edu>
2023-07-20 14:57:44 -0400

    add generateRandomPosition in Driver.java

--- Project/src/Project/Driver.java
+++ Project/src/Project/Driver.java
@@ -1,7 +1,9 @@
 package Project;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Driver {
     List<Request> schedule;
@@ -19,4 +21,26 @@ public class Driver {
     public void setPosition(double[] new_position) {
     	this.position = new_position;
     }
+    
+    public double[] generateRandomPosition() {
+    	double[] res = new double[2];
+		Random rand = new Random();
+
+        double x = rand.nextDouble(100); // Generate first double
+        double y = rand.nextDouble(100);
+        
+        res[0] = x;
+        res[1] = y;
+        
+        return res;
+        
+    }
+    public static void main(String[] args) {
+    	double[] origin = new double[2];
+    	origin[0] = 0;
+    	origin[1] = 0;
+    	Driver test = new Driver(origin);
+    	System.out.println(Arrays.toString(test.generateRandomPosition()));
+    }
+    
 }
