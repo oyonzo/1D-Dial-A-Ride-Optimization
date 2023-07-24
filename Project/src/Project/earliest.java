ea419d6f930a51e02ffacbdec380693915ef7dc2
andrewdean1 <dean@middlebury.edu>
2023-07-24 15:48:41 -0400

    updates to earliest.javva

--- Project/src/Project/earliest.java
+++ Project/src/Project/earliest.java
@@ -16,7 +16,7 @@ public class earliest {
         Collections.sort(requestList, Comparator.comparingDouble(request -> request.pickTime));
        
         // Step 3: Assign each driver the (at most) n/k requests with the closest pickup locations
-        assignClosestRequests(driverList, requestList, n/k);
+        assignClosestRequests(driverList, unassignedRequests, n/k, assignedDrivers);
 
         // Step 4: For any unassigned request, find the closest driver and assign
         for (Request request : unassignedRequests) {
@@ -34,17 +34,18 @@ public class earliest {
             // for an assigned driver
             if (d != null) {
                 // if pickup time hasn't passed and enough time to drive to pickup location, add request e to schedule
-                if (e.pickTime > currentTime() && hasEnoughTime(d, e)) {
+                if (e.pickTime > d.getCurrentTime() && hasEnoughTime(d, e)) {
                     d.schedule.add(e);
                     // update driver's currLocation to finishPos of completed request
                     d.setPosition(e.finishPos);
+                    d.setCurrentTime(e.finishTime);
                 }
             }
         }
     }
 
     // assigns closest requests to driver
-    private void assignClosestRequests(List<Driver> drivers, List<Request> requests, int count) {
+    private void assignClosestRequests(List<Driver> drivers, List<Request> requests, int count, Map<Request, Driver> assignedDrivers) {
         for (Driver driver : drivers) {
             // create a priority queue to hold the requests sorted by distance
             PriorityQueue<Request> closestRequests = new PriorityQueue<>(
@@ -56,7 +57,13 @@ public class earliest {
             // assign the closest requests to the driver
             for (int i = 0; i < count && !closestRequests.isEmpty(); i++) {
                 Request request = closestRequests.poll();  // get the closest request
-                driver.schedule.add(request);  // add it to the driver's schedule
+                if(assignedDrivers.containsKey(request)) {
+                	if (Graph.dist(driver.getPosition(),request.startPos) < Graph.dist(assignedDrivers.get(request).getPosition(), request.startPos)) {
+                		assignedDrivers.put(request, driver);
+                	}
+                } else {
+                	assignedDrivers.put(request, driver);
+                }
                 requests.remove(request);  // remove it from the list of requests
             }
         }
@@ -81,8 +88,8 @@ public class earliest {
     // calculate if the driver has enough time to get from their current position to the pickup position of a request
     // @return true if there is enough time, false otherwise
     public boolean hasEnoughTime (Driver d, Request r) {
-        double drivingTime = Graph.dist(d.getPosition(), r.startPos) / 1;
-        return (currentTime() + drivingTime <= r.pickTime);
+        double drivingTime = Graph.dist(d.getPosition(), r.startPos) / 1; // Calculate driving time
+        return (d.getCurrentTime() + drivingTime <= r.pickTime); // Return true if driver can arrive on time
     }
 
     // @return driver of specified request
@@ -94,11 +101,6 @@ public class earliest {
         }
         return null;
     }*/
-
-    private double currentTime() {
-        // Actual implementation depends on system specifics
-        return 0;
-    }
 }
 
 
