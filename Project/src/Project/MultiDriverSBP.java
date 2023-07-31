bf39850d816f5fe9de9cf9f362ec82c3e5915794
yb <95590361+oyonzo@users.noreply.github.com>
2023-07-31 11:41:24 -0400

    list to hash method and start of an assignrequests method

--- Project/src/Project/MultiDriverSBP.java
+++ Project/src/Project/MultiDriverSBP.java
@@ -1,6 +1,6 @@
 package Project;
 
-import java.util.List;
+import java.util.*;
 
 public class MultiDriverSBP {
     int TLIMIT = Graph.TLIMIT;
@@ -20,8 +20,41 @@ public class MultiDriverSBP {
         }
         return rl;
     }
-    // TODO: Make a method to convert the list of requests into a hashmap
-    // TODO: Make a function that recursively compares the optimal sets for each
+
+    public static Map<Request, Integer> rltoHash(List<Request> rl) {
+        Map<Request, Integer> map = new HashMap<>();
+
+        // Loop to generate keys "r1", "r2", "r3", ...
+        for (int i = 0; i <= rl.size(); i++) {
+            Request request = rl.get(i);
+            if(map.containsKey(request){
+                map.replace(request, map.get(request) + 1);
+            }
+            else{
+                map.put(request, 1);
+            }
+        }
+        return map;
+    }
+
+    // TODO: Make it so that we work with maps of requests instead of lists of
+    // requests
+    public static List<Driver> assignRequests(List<Driver> drivers, Map<Request, Integer> requests) {
+        int largestSched = 0;
+        Driver largestDriver = null;
+        List<Request> lrl = new ArrayList<Request>(requests.keySet());
+        for (Driver d : drivers) {
+            List<Request> currSched = Driver.getOptimalforDriver(lrl, d)
+            int currSchedSize = currSched.size();
+            if (currSchedSize > largestSched) {
+                largestSched = currSchedSize;
+                largestDriver = d;
+                d.schedule = currSched;
+            }
+
+        }
+        return ;
+    }
     // driver and finds the longest one and then removes the requests in the set as
     // well as the driver
     // ^^ perhaps this would take a hashmap of requests and a list of drivers as its
